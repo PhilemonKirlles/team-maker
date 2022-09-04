505c0b9f57980f3705c3ee45b67a86b40e637254
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-09-04 10:08:25 -0400

    index and inquirer

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,12 +2,12 @@ const Intern = require("../lib/Intern");
 
 test("Ability to set school using constructor function", () => {
     const school = "UConn School of Dreams ";
-    const employee = new Intern("Philemon", 1, "Philemon.faria@gmail.com", school);
+    const employee = new Intern("Philemon", 1, "Philemon.kirlleuos@gmail.com", school);
     expect(employee.school).toBe(school);
   });
 
   test("getRole() should return Intern as a role", () => {
     const role = "Intern";
-    const employee = new Intern("Philemon", 1, "Philemon.faria@gmail.com", "Philemonfaria");
+    const employee = new Intern("Philemon", 1, "Philemon.kirlleuos@gmail.com", "Philemonkirlleuos");
     expect(employee.getRole()).toBe(role);
   });
