46e3a245d3fc7242aadac4adf7072f3c0e0fe87d
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-09-04 16:09:07 -0400

    resolving test issues

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,23 +3,26 @@ const Intern = require("../lib/Intern");
 test("create intern object", () => {
   const name = "Alexandra";
   const id = "3000";
-  const email = "Alexandra@gmail.com";
-  const school = "Github School of Zeros and Ones";
-  const intern = new Intern(name, id, email, school);
+  const email = "alexandra@gmail.com";
+
+  const intern = new Intern(name, id, email);
 
   expect(intern.name).toBe(name);
   expect(intern.id).toBe(id);
-  expect(intern.school).toBe(school);
   expect(intern.email).toBe(email);
+
 });
 
 test("retrieve intern school", () => {
+
   const name = "Alexandra";
   const id = "3000";
   const school = "Github School of Zeros and Ones";
 
-  const intern = new Intern(name, id, email, school);
   const email = "Alexandra@gmail.com";
+  const intern = new Intern(name, id, school, email);
+
+
   expect(intern.getSchool()).toStrictEqual(
     expect.stringContaining(intern.school.toString())
   );
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,14 +3,13 @@ const Manager  = require('../lib/Manager');
 test('create manager object', () => {
     const name = 'Biden';
     const id = '4000';
-    const officeNumber = '400036'
-    const manager = new Manager(name,id,email,officeNumber);
     const email = 'Biden@gmail.com';
 
     expect(manager.name).toBe(name);
     expect(manager.id).toBe(id);
-    expect(manager.officeNumber).toBe(officeNumber);
     expect(manager.email).toBe(email);
+    const manager = new Manager(name, id, email);
+
 
 })
 test('retrieve manager office number', () => {
@@ -18,7 +17,6 @@ test('retrieve manager office number', () => {
     const id = '4000';
     const officeNumber = '400036'
     const manager = new Manager(name,id,email,officeNumber);
-    const email = 'Biden@gmail.com';
    
 
     expect(manager.getofficeNumber()).toStrictEqual(expect.stringContaining(manager.officeNumber.toString()));
