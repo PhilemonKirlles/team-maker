09720c510c789e7bb15aaa16914b17e294b17010
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-09-05 12:17:50 -0400

    css works

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,19 +5,23 @@ test('create manager object', () => {
     const id = '4000';
     const email = 'Biden@gmail.com';
 
+    const manager = new Manager(name, id, email);
+
     expect(manager.name).toBe(name);
     expect(manager.id).toBe(id);
     expect(manager.email).toBe(email);
-    const manager = new Manager(name, id, email);
+    
 
 
 })
 test('retrieve manager office number', () => {
     const name = 'Biden';
     const id = '4000';
+    const email = 'Biden@gmail.com';
     const officeNumber = '400036'
+    
     const manager = new Manager(name,id,email,officeNumber);
    
 
-    expect(manager.getofficeNumber()).toStrictEqual(expect.stringContaining(manager.officeNumber.toString()));
+    expect(manager.getOfficeNumber()).toStrictEqual(expect.stringContaining(manager.officeNumber.toString()));
 })
